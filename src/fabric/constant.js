9f579cd2f9247978bba99d808f609b9a3954df86
Attila Klenik <a.klenik@gmail.com>
2018-04-12 01:55:31 +0200

    Fixed Fabric Tx life-cycle handling.
    Signed-off-by: Attila Klenik <a.klenik@gmail.com>

--- src/fabric/constant.js
+++ src/fabric/constant.js
@@ -1,10 +1,31 @@
-'use strict'
+'use strict';
 
 var os   = require('os');
 var path = require('path');
 
 var tempdir = path.join(os.tmpdir(), 'hfc');
 
+const TxErrorEnum = {
+    NoError: 0,
+    ProposalResponseError: 1,
+    BadProposalResponseError: 2,
+    OrdererResponseError: 4,
+    BadOrdererResponseError: 8,
+    EventNotificationError: 16,
+    BadEventNotificationError: 32
+};
+
+const TxErrorIndex = {
+    ProposalResponseError: 0,
+    BadProposalResponseError: 1,
+    OrdererResponseError: 2,
+    BadOrdererResponseError: 3,
+    EventNotificationError: 4,
+    BadEventNotificationError: 5
+};
+
 module.exports = {
-    tempdir: tempdir
-}
+    tempdir: tempdir,
+    TxErrorIndex: TxErrorIndex,
+    TxErrorEnum: TxErrorEnum
+};
