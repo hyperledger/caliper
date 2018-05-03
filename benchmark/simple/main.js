994798a21fce04d5d762ab3caefdbba6add41510
Subhod I <subhodpoojary@gmail.com>
2018-05-03 11:51:31 +0530

    Enforce linting on benchmark/fabric files
    Signed-off-by: Subhod I <subhodpoojary@gmail.com>

--- benchmark/simple/main.js
+++ benchmark/simple/main.js
@@ -6,28 +6,40 @@
 */
 
 
-'use strict'
+'use strict';
 
-var configFile;
-var networkFile;
+let configFile;
+let networkFile;
+
+/**
+ * Set benchmark config file
+ * @param {*} file config file of the benchmark,  default is config.json
+ */
 function setConfig(file) {
     configFile = file;
 }
 
+/**
+ * Set benchmark network file
+ * @param {*} file config file of the blockchain system, eg: fabric.json
+ */
 function setNetwork(file) {
     networkFile = file;
 }
 
+/**
+ * Entry point of the Benchmarking script.
+ */
 function main() {
-    var program = require('commander');
+    let program = require('commander');
     program.version('0.1')
         .option('-c, --config <file>', 'config file of the benchmark, default is config.json', setConfig)
         .option('-n, --network <file>', 'config file of the blockchain system under test, if not provided, blockchain property in benchmark config is used', setNetwork)
         .parse(process.argv);
 
-    var path = require('path');
-    var fs = require('fs-extra');
-    var absConfigFile;
+    const path = require('path');
+    const fs = require('fs-extra');
+    let absConfigFile;
     if(typeof configFile === 'undefined') {
         absConfigFile = path.join(__dirname, 'config.json');
     }
@@ -39,8 +51,8 @@ function main() {
         return;
     }
 
-    var absNetworkFile;
-    var absCaliperDir = path.join(__dirname, '../..');
+    let absNetworkFile;
+    let absCaliperDir = path.join(__dirname, '../..');
     if(typeof networkFile === 'undefined') {
         try{
             let config = require(absConfigFile);
@@ -60,7 +72,7 @@ function main() {
     }
 
 
-    var framework = require('../../src/comm/bench-flow.js');
+    const framework = require('../../src/comm/bench-flow.js');
     framework.run(absConfigFile, absNetworkFile);
 }
 
--- benchmark/simple/open.js
+++ benchmark/simple/open.js
@@ -5,34 +5,35 @@
 *
 */
 
-'use strict'
+'use strict';
 
-module.exports.info  = "opening accounts";
+module.exports.info  = 'opening accounts';
 
-var accounts = [];
-var initMoney;
-var bc, contx;
+let accounts = [];
+let initMoney;
+let bc, contx;
 module.exports.init = function(blockchain, context, args) {
     if(!args.hasOwnProperty('money')) {
-        return Promise.reject(new Error("simple.open - 'money' is missed in the arguments"));
+        return Promise.reject(new Error('simple.open - "money" is missed in the arguments'));
     }
 
     initMoney = args['money'].toString();
     bc = blockchain;
     contx = context;
     return Promise.resolve();
-}
+};
 
-var dic = 'abcdefghijklmnopqrstuvwxyz';
+const dic = 'abcdefghijklmnopqrstuvwxyz';
 function get26Num(number){
-    var result = '';
+    let result = '';
     while(number > 0) {
         result += dic.charAt(number % 26);
         number = parseInt(number/26);
     }
     return result;
 }
-var prefix;
+
+let prefix;
 function generateAccount() {
     // should be [a-z]{1,9}
     if(typeof prefix === 'undefined') {
@@ -42,13 +43,13 @@ function generateAccount() {
 }
 
 module.exports.run = function() {
-    var newAcc = generateAccount();
+    let newAcc = generateAccount();
     accounts.push(newAcc);
     return bc.invokeSmartContract(contx, 'simple', 'v0', {verb: 'open', account: newAcc, money: initMoney}, 30);
-}
+};
 
 module.exports.end = function(results) {
     return Promise.resolve();
-}
+};
 
 module.exports.accounts = accounts;
