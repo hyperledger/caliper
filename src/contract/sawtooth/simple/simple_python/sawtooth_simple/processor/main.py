b1a2daab0000ad5377a530236e8c91a080dc7751
James King <james.king@emc.com>
2018-07-17 11:25:56 -0400

    Updated Sawtooth Simple Example to Work
    Signed-off-by: Justin Kenney <justin.kenney@emc.com>

--- src/contract/sawtooth/simple/simple_python/sawtooth_simple/processor/main.py
+++ src/contract/sawtooth/simple/simple_python/sawtooth_simple/processor/main.py
@@ -61,22 +61,22 @@ def main(args=None):
     processor = None
     try:
         processor = TransactionProcessor(url=opts.connect)
-        log_config = get_log_config(filename="intkey_log_config.toml")
-
-        # If no toml, try loading yaml
-        if log_config is None:
-            log_config = get_log_config(filename="intkey_log_config.yaml")
-
-        if log_config is not None:
-            log_configuration(log_config=log_config)
-        else:
-            log_dir = get_log_dir()
-            # use the transaction processor zmq identity for filename
-            log_configuration(
-                log_dir=log_dir,
-                name="intkey-" + str(processor.zmq_id)[2:-1])
-
-        init_console_logging(verbose_level=opts.verbose)
+        # log_config = get_log_config(filename="simple_log_config.toml")
+        #
+        # # If no toml, try loading yaml
+        # if log_config is None:
+        #     log_config = get_log_config(filename="simple_log_config.yaml")
+        # 
+        # if log_config is not None:
+        #     log_configuration(log_config=log_config)
+        # else:
+        #     log_dir = get_log_dir()
+        #     # use the transaction processor zmq identity for filename
+        #     log_configuration(
+        #         log_dir=log_dir,
+        #         name="simple-" + str(processor.zmq_id)[2:-1])
+        #
+        # init_console_logging(verbose_level=opts.verbose)
 
         # The prefix should eventually be looked up from the
         # validator's namespace registry.
