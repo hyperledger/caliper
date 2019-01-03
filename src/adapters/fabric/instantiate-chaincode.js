89b3fb281ba0ac685196f0953d8cd794684d5b95
nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>
2019-01-03 14:07:42 +0000

    Enable use of 1.3+ client and retain legacy code for fabric 1.0 and 1.1
    Signed-off-by: nkl199@yahoo.co.uk <nkl199@yahoo.co.uk>

--- src/adapters/fabric/instantiate-chaincode.js
+++ src/adapters/fabric/instantiate-chaincode.js
@@ -1,30 +1,32 @@
 /**
- * Modifications Copyright 2017 HUAWEI
- * Copyright 2017 IBM All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
-// This is an end-to-end test that focuses on exercising all parts of the fabric APIs
-// in a happy-path scenario
 'use strict';
 
 const e2eUtils = require('./e2eUtils.js');
 const commUtils = require('../../comm/util');
 const commLogger = commUtils.getLogger('instantiate-chaincode.js');
 
-module.exports.run = async function (config_path) {
-    const fabricSettings = commUtils.parseYaml(config_path).fabric;
+/**
+ * Install the chaincode listed within config
+ * @param {*} config_path The path to the Fabric network configuration file.
+ * @async
+ */
+async function run(config_path) {
+    const config = commUtils.parseYaml(config_path);
+    const fabricSettings = config.fabric;
     const policy = fabricSettings['endorsement-policy'];  // TODO: support multiple policies
     let chaincodes = fabricSettings.chaincodes;
     if(typeof chaincodes === 'undefined' || chaincodes.length === 0) {
@@ -44,4 +46,6 @@ module.exports.run = async function (config_path) {
         commLogger.error(`Failed to instantiate chaincodes: ${(err.stack ? err.stack : err)}`);
         throw err;
     }
-};
+}
+
+module.exports.run = run;
