e1ec27240ee4717f28876d604514dfaf9c20cd84
Jasonyou1995 <jason.you1995@gmail.com>
2020-02-27 07:18:36 -0500

    resolving alerts caused by fonts, adding caliper-gui packages to lerna, and fix version mismatches
    Signed-off-by: Jasonyou1995 <jason.you1995@gmail.com>

---
+++ packages/caliper-gui-server/test/random-gen-test.js
@@ -0,0 +1,8 @@
+let dataConstructor = require('../src/utility/random-data-generator.js').dataConstructor;
+
+data = new dataConstructor();
+
+console.log(data);
+
+// data.refresh();
+// console.log(`data: ${data}`);
