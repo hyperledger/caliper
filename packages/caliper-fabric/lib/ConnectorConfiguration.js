bb9f7b16fd2a59556a752b25917ad878600df253
Dave Kelsey <d_kelsey@uk.ibm.com>
2020-08-20 11:24:24 +0100

    initial connector configuration support (#952)
    * initial connector configuration support
    
    Signed-off-by: D Kelsey <d_kelsey@uk.ibm.com>
    
    * added in newlines to end of files
    
    Signed-off-by: D Kelsey <d_kelsey@uk.ibm.com>

---
+++ packages/caliper-fabric/lib/ConnectorConfiguration.js
@@ -0,0 +1,36 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const CaliperUtils = require('@hyperledger/caliper-core').CaliperUtils;
+
+/**
+ * Parse and process the fabric connector configuration
+ */
+class ConnectorConfiguration {
+
+    /**
+     * Constructor
+     * @param {*} connectorConfigurationPath path to the json or yaml file defining the configuration
+     */
+    constructor(connectorConfigurationPath) {
+        CaliperUtils.assertDefined(connectorConfigurationPath, '[ConnectorConfiguration.constructor] Parameter \'connectorConfigurationPath\' is undefined or null');
+
+        const configPath = CaliperUtils.resolvePath(connectorConfigurationPath);
+        this.adapterConfiguration = CaliperUtils.parseYaml(configPath);
+    }
+}
+
+module.exports = ConnectorConfiguration;
