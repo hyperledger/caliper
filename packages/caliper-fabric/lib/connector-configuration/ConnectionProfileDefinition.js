1de30a538ee240757f1d8f81e28d66c35eb767b7
Dave Kelsey <d_kelsey@uk.ibm.com>
2020-09-24 10:42:46 +0100

    Add support for isTLSEnabled (#1010)

--- packages/caliper-fabric/lib/connector-configuration/ConnectionProfileDefinition.js
+++ packages/caliper-fabric/lib/connector-configuration/ConnectionProfileDefinition.js
@@ -24,7 +24,13 @@ class ConnectionProfileDefinition {
      */
     constructor(ConnectionProfileConfiguration) {
         this.connectionProfile = ConnectionProfileConfiguration.loadedConnectionProfile;
-        this.dynamicConnectionProfile = ConnectionProfileConfiguration.discover;
+        this.dynamicConnectionProfile = typeof ConnectionProfileConfiguration.discover === 'boolean' ? ConnectionProfileConfiguration.discover : false;
+        this.TLSEnabled = false;
+
+        if (this._searchForPropertyValues(this.connectionProfile, 'url', /grpcs|https/).length > 0) {
+            this.TLSEnabled = true;
+        }
+
     }
 
     /**
@@ -40,6 +46,43 @@ class ConnectionProfileDefinition {
     isDynamicConnectionProfile() {
         return this.dynamicConnectionProfile;
     }
+
+    /**
+     * Returns whether the connection profile is using TLS somewhere or not
+     * @returns {boolean} true if at least 1 entry has grpcs or https
+     */
+    isTLSEnabled() {
+        return this.TLSEnabled;
+    }
+
+    /**
+     * Search for a property name, whose value matches the regex
+     * Don't need to handle arrays at this time
+     *
+     * @param {*} object the object to search and update
+     * @param {string} propertyName a property name in the configuration
+     * @param {regexp} propertyValueMatch a regex pattern the value has to match against
+     * @returns {[*]} an array of the object properties that have a matching value
+     *
+     */
+    _searchForPropertyValues(object, propertyName, propertyValueMatch) {
+        const foundProperties = [];
+
+        for (const objectKey in object) {
+            if (objectKey === propertyName &&
+                propertyValueMatch.test(object[objectKey])) {
+                foundProperties.push(object[objectKey]);
+            } else {
+                if (typeof object[objectKey] === 'object') {
+                    foundProperties.push(...this._searchForPropertyValues(object[objectKey], propertyName, propertyValueMatch));
+                }
+            }
+
+        }
+
+        return foundProperties;
+    }
+
 }
 
 module.exports = ConnectionProfileDefinition;
