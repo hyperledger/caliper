1b9d108f81ea55c257f00a3dbfe7a27451a4c62d
fraVlaca <ocsenarf@outlook.com>
2022-01-20 17:24:17 +0000

    fix memory usage stat of the docker-monitor
    Signed-off-by: fraVlaca <ocsenarf@outlook.com>

--- packages/caliper-core/lib/manager/monitors/monitor-docker.js
+++ packages/caliper-core/lib/manager/monitors/monitor-docker.js
@@ -201,8 +201,9 @@ class MonitorDocker extends MonitorInterface {
                     }
                     if (this.containers[i].remote === null) {
                         // local
-                        this.stats[id].mem_usage.push(stat.mem_usage);
-                        this.stats[id].mem_percent.push(stat.mem_percent);
+                        const actualMemUsage = stat.memory_stats.usage - stat.memory_stats.stats.cache;
+                        this.stats[id].mem_usage.push(actualMemUsage);
+                        this.stats[id].mem_percent.push(actualMemUsage / stat.mem_limit);
                         let cpuDelta = stat.cpu_stats.cpu_usage.total_usage - stat.precpu_stats.cpu_usage.total_usage;
                         let sysDelta = stat.cpu_stats.system_cpu_usage - stat.precpu_stats.system_cpu_usage;
                         if (cpuDelta > 0 && sysDelta > 0) {
@@ -220,8 +221,9 @@ class MonitorDocker extends MonitorInterface {
                         this.stats[id].blockIO_wx.push(stat.blockIO.w);
                     } else {
                         // remote
-                        this.stats[id].mem_usage.push(stat.memory_stats.usage);
-                        this.stats[id].mem_percent.push(stat.memory_stats.usage / stat.memory_stats.limit);
+                        const actualMemUsage = stat.memory_stats.usage - stat.memory_stats.stats.cache;
+                        this.stats[id].mem_usage.push(actualMemUsage);
+                        this.stats[id].mem_percent.push(actualMemUsage / stat.mem_limit);
                         //this.stats[id].cpu_percent.push((stat.cpu_stats.cpu_usage.total_usage - stat.precpu_stats.cpu_usage.total_usage) / (stat.cpu_stats.system_cpu_usage - stat.precpu_stats.system_cpu_usage) * 100);
                         let cpuDelta = stat.cpu_stats.cpu_usage.total_usage - stat.precpu_stats.cpu_usage.total_usage;
                         let sysDelta = stat.cpu_stats.system_cpu_usage - stat.precpu_stats.system_cpu_usage;
@@ -361,10 +363,12 @@ class MonitorDocker extends MonitorInterface {
                     watchItemStat.set('Memory(max)', mem_stat.max);
                     watchItemStat.set('Memory(avg)', mem_stat.avg);
                     if(this.options.hasOwnProperty('cpuUsageNormalization')){
-                        cpuCores = os.cpus().length;
+                        if(this.options.cpuUsageNormalization){
+                            cpuCores = os.cpus().length;
+                        }
                     }
-                    watchItemStat.set('CPU%(max)', cpu_stat.max.toFixed(2))/cpuCores;
-                    watchItemStat.set('CPU%(avg)', cpu_stat.avg.toFixed(2))/cpuCores;
+                    watchItemStat.set('CPU%(max)', cpu_stat.max.toFixed(2)/cpuCores);
+                    watchItemStat.set('CPU%(avg)', cpu_stat.avg.toFixed(2)/cpuCores);
                     watchItemStat.set('Traffic In', (net.in[net.in.length - 1] - net.in[0]));
                     watchItemStat.set('Traffic Out', (net.out[net.out.length - 1] - net.out[0]));
                     watchItemStat.set('Disc Write', (disc.write[disc.write.length - 1] - disc.write[0]));
