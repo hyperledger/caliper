193d927abfa5babb885e73c5df51cbbc985f6800
D <d_kelsey@uk.ibm.com>
2022-03-22 14:48:17 +0000

    Remove CountQueryAsLoad Option
    Also fixes
    1. unused timeout property in fabric gateway connectors
    2. ignore license check in generator tests for downloaded files
    
    Doc update to follow
    
    Signed-off-by: D <d_kelsey@uk.ibm.com>

--- packages/caliper-fabric/lib/connector-versions/v1/FabricGateway.js
+++ packages/caliper-fabric/lib/connector-versions/v1/FabricGateway.js
@@ -89,9 +89,7 @@ class V1FabricGateway extends ConnectorBase {
         this.context = undefined;
 
         // this value is hardcoded, if it's used, that means that the provided timeouts are not sufficient
-        this.configSmallestTimeout = 1000;
         this.configDefaultTimeout = ConfigUtil.get(ConfigUtil.keys.Fabric.Timeout.InvokeOrQuery, 60);
-        this.configCountQueryAsLoad = ConfigUtil.get(ConfigUtil.keys.Fabric.CountQueryAsLoad, true);
 
         // Gateway connector
         this.configLocalHost = ConfigUtil.get(ConfigUtil.keys.Fabric.Gateway.LocalHost, true);
