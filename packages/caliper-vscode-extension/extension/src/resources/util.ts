21bb9c03ecb899c914ec1b496a7a37e8ae7bd02a
eravatee <eraraje1997@gmail.com>
2022-07-26 02:19:51 -0600

    remove extension folder
    Signed-off-by: eravatee <eraraje1997@gmail.com>

--- packages/caliper-vscode-extension/extension/src/resources/util.ts
+++
@@ -1,7 +0,0 @@
-export interface GeneralObject {
-    [index: string]: object;
-  }
-  export const extractAsKeyValue = (object: GeneralObject) => ({
-    key: Object.keys(object)[0],
-    value: Object.values(object)[0],
-  });
