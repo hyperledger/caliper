477b68be3e3e757f4c4d965799b5ae422b11280d
Eravatee Raje <eraraje1997@gmail.com>
2022-10-03 03:31:10 -0600

    Add VSCode extension skeleton and runtime config schema (#1404)
    * JSON schema file for runtime configuration validations
    * Basic skeleton for vscode extension to implement further JSON schemas and UI elements
    
    Signed-off-by: eravatee <eraraje1997@gmail.com>

--- packages/caliper-vscode-extension/src/extension.ts
+++ packages/caliper-vscode-extension/src/extension.ts
@@ -1,15 +1,6 @@
 import * as vscode from 'vscode';
-// import { ConfigFileProvider, ConfigFile } from './ConfigListProvider';
 import { ConfigFileExplorer } from './ConfigFileExplorer';
 export function activate(context: vscode.ExtensionContext) {
-
-    // const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
-    // ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
-    // const configScriptsProvider = new ConfigFileProvider(rootPath);
-    // vscode.window.createTreeView('configScripts', {
-    //     treeDataProvider: new ConfigFileProvider(rootPath)
-    //   });
-	// vscode.commands.registerCommand('configScripts.refreshEntry', () => configScriptsProvider.refresh());
 
     new ConfigFileExplorer(context);
 }
