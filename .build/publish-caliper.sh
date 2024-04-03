4bdb98cd573df4ed96454f47d56a4690d4c38c54
Dave Kelsey <d_kelsey@uk.ibm.com>
2024-04-03 17:53:54 +0100

    downgrade npm on publish as temp fix (#1523)
    downgrade the version of npm on the publish step to ensure that publish
    doesn't give an ENOWORKSPACES error
    
    This should be considered a temporary solution to getting publish
    working again and a better solution needs to be found.
    
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- .build/publish-caliper.sh
+++ .build/publish-caliper.sh
@@ -24,5 +24,7 @@ cp ./README.md ./packages/caliper-fabric/README.md
 
 cd ./packages/caliper-publish/
 npm ci
+# temporary workaround to downgrade npm in order to publish
+npm install -g npm@8.19.4
 ./publish.js npm
 ./publish.js docker --publish
