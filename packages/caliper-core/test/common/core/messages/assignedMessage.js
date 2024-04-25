2d3ed62de79c94f7ec65ba11ac88c21bca7ed80a
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-04-26 00:30:28 +0530

    add test
    Signed-off-by: Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>

--- packages/caliper-core/test/common/core/messages/assignedMessage.js
+++ packages/caliper-core/test/common/core/messages/assignedMessage.js
@@ -1,12 +1,12 @@
 /*
-* Licensed under the Apache License, Version 2.0 (the "License");
+* Licensed under the Apache License, Version 2.0 (the 'License');
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
 * http://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
+* distributed under the License is distributed on an 'AS IS' BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
@@ -33,12 +33,20 @@ describe('AssignedMessage', () => {
             message.type.should.equal(MessageTypes.Assigned);
         });
 
+        it('should set the content of the message as an empty object', () => {
+            const message = new AssignedMessage('John', ['Alice', 'Bob']);
+
+            message.content.should.deep.equal({});
+        });
+
         it('should parse the date argument as a Date object for the AssignedMessage', () => {
             const date = '2024-04-25';
             const message = new AssignedMessage('John', ['Alice', 'Bob'], date);
 
             message.date.should.be.an.instanceOf(Date);
-            message.date.toISOString().should.equal(new Date(date).toISOString());
+            message.date
+                .toISOString()
+                .should.equal(new Date(date).toISOString());
         });
 
         it('should set the date as an Invalid Date object if the date string is not a valid date', () => {
@@ -51,7 +59,12 @@ describe('AssignedMessage', () => {
 
         it('should set the error correctly if passed', () => {
             const error = 'Some error';
-            const message = new AssignedMessage('John', ['Alice', 'Bob'], undefined, error);
+            const message = new AssignedMessage(
+                'John',
+                ['Alice', 'Bob'],
+                undefined,
+                error
+            );
 
             message.error.should.equal(error);
         });
